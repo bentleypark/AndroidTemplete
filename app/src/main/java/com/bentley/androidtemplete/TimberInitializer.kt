0a8fcab30a5c8d8c8a8dcd4280a68496f74cd18d
Bentley Park <junggil.park@sovoro.kr>
2020-10-03 20:40:29 +0900

    add base module and activite Timber using app startup

---
+++ app/src/main/java/com/bentley/androidtemplete/TimberInitializer.kt
@@ -0,0 +1,18 @@
+package com.bentley.androidtemplete
+
+import android.content.Context
+import androidx.startup.Initializer
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        Timber.d("TimberInitializer is initialized.")
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> =
+        emptyList()
+}
