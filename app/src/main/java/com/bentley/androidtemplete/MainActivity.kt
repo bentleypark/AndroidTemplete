41d23bfec1423af8d1220c50b446511b91a1a30c
Bentley Park <junggil.park@sovoro.kr>
2020-10-05 11:30:53 +0900

    Add AppCompatActivity Viewbinding Ex Funtion

--- app/src/main/java/com/bentley/androidtemplete/MainActivity.kt
+++ app/src/main/java/com/bentley/androidtemplete/MainActivity.kt
@@ -2,10 +2,15 @@ package com.bentley.androidtemplete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bentley.androidtemplete.databinding.ActivityMainBinding
+import com.bentley.common.ex.viewBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.tvTitle.text = "View Binding"
     }
 }
