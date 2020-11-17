c0ac03e8a7d5108075acc1b705b39f1c2ef021d6
Bentley Park <junggil.park@sovoro.kr>
2020-11-17 11:39:30 +0900

    [feature-stt] add SpeechRecognizerOberser class

--- app/src/main/java/com/bentley/androidtemplete/MainActivity.kt
+++ app/src/main/java/com/bentley/androidtemplete/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.bentley.androidtemplete
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.bentley.androidtemplete.databinding.ActivityMainBinding
 import com.bentley.common.ex.viewBinding
-import com.bentley.data.model.Blog
-import com.bentley.data.state.DataState
 import org.koin.androidx.fragment.android.setupKoinFragmentFactory
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.KoinExperimentalAPI
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
     private val viewModel: MainViewModel by viewModel()
+
+//    private val permissions = arrayOf(
+//        Manifest.permission.RECORD_AUDIO,
+////        Manifest.permission.WRITE_EXTERNAL_STORAGE
+//    )
+//
+//    private val requestPermission = registerForActivityResult(
+//        ActivityResultContracts.RequestPermission()
+//    ) { isGranted ->
+//        if (isGranted) {
+//            // action to do something
+//        } else {
+//
+//        }
+//    }
 
     @KoinExperimentalAPI
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,21 +36,18 @@ class MainActivity : AppCompatActivity() {
         setupKoinFragmentFactory()
         setContentView(binding.root)
 
-        viewModel.setStateEvent(MainStateEvent.GetBlogsEvent)
 
-        viewModel.dataState.observe(this,
-            { dataState ->
-                when (dataState) {
-                    is DataState.Success<List<Blog>> -> {
-                        Timber.d(dataState.data.size.toString())
-                    }
-                }
-            }
-        )
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
+//        requestPermission.launch(Manifest.permission.RECORD_AUDIO)
+//        viewModel.setStateEvent(MainStateEvent.GetBlogsEvent)
+//
+//        viewModel.dataState.observe(this,
+//            { dataState ->
+//                when (dataState) {
+//                    is DataState.Success<List<Blog>> -> {
+//                        Timber.d(dataState.data.size.toString())
+//                    }
+//                }
+//            }
+//        )
     }
 }
--- feature-stt/src/main/java/kr/sovoro/readbean/ui/STTFragment.kt
+++ feature-stt/src/main/java/kr/sovoro/readbean/ui/STTFragment.kt
@@ -1,36 +1,47 @@
 package kr.sovoro.readbean.ui
 
 import android.Manifest
-import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.bentley.common.ex.askPermission
-import com.bentley.common.ex.isHasPermission
-import kr.sovoro.readbean.R
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import com.bentley.common.base.ViewBindingHolder
+import com.bentley.common.base.ViewBindingHolderImpl
+import kr.sovoro.readbean.databinding.FragmentSttBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 import java.util.*
 
-class STTFragment : Fragment(), RecognitionListener {
+class STTFragment : Fragment(),
+    ViewBindingHolder<FragmentSttBinding> by ViewBindingHolderImpl() {
 
     private val viewModel: STTViewModel by viewModel()
 
     private var isTranslationOngoing = false
     private val permissions = arrayOf(
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
+        Manifest.permission.RECORD_AUDIO
     )
 
+    private val sttAcitivity = registerForActivityResult(STTAcitivityContract()) { result ->
+        if (result!!.isNotEmpty()) {
+            binding!!.apply {
+                textFinal.text = result
+            }
+        }
+    }
+
     private val askMultiplePermissions =
-        requireActivity().registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
             for (entry in map.entries) {
                 Toast.makeText(
                     requireContext(),
@@ -40,93 +51,98 @@ class STTFragment : Fragment(), RecognitionListener {
             }
         }
 
+    lateinit var observer: SpeechRecognizerObserver
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+
+        askMultiplePermissions.launch(permissions)
+        observer = SpeechRecognizerObserver(requireContext())
+        lifecycle.addObserver(observer)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View? = initBinding(FragmentSttBinding.inflate(layoutInflater), this) {
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        observer.selectImage()
 
-    }
-
-    override fun onReadyForSpeech(params: Bundle?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBeginningOfSpeech() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onRmsChanged(rmsdB: Float) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBufferReceived(buffer: ByteArray?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onEndOfSpeech() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onError(error: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onResults(results: Bundle?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onPartialResults(partialResults: Bundle?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onEvent(eventType: Int, params: Bundle?) {
-        TODO("Not yet implemented")
-    }
-
-    private fun checkPermissions() {
-        if (!requireActivity().isHasPermission(*permissions)) {
-            requireActivity()
+        binding!!.btnTranslate.setOnClickListener {
+//            sttAcitivity.launch("STT Test")
+            observer.speechRecognizer.startListening(createIntent(requireContext()))
         }
     }
 
-    private fun startTranslation() {
+    private fun createIntent(context: Context): Intent {
 
-        isTranslationOngoing = true
-
-        val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+        return Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
             putExtra(
                 RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
             )
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, requireContext().packageName)
+            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, context.packageName)
             putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
             putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1)
         }
-
-        try {
-            startActivityForResult(sttIntent, REQUEST_CODE_STT)
-        } catch (e: ActivityNotFoundException) {
-            e.printStackTrace()
-            Timber.e(e)
-        }
-
-
     }
 
     companion object {
         fun newInstance() = STTFragment()
 
         const val REQUEST_CODE_STT = 1
+    }
+}
+
+class SpeechRecognizerObserver(private val context: Context) : RecognitionListener,
+    DefaultLifecycleObserver {
+
+    lateinit var speechRecognizer: SpeechRecognizer
+
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+        Timber.d("onCreate")
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context)
+        speechRecognizer.setRecognitionListener(this)
+    }
+
+    override fun onReadyForSpeech(params: Bundle?) {
+        Timber.d("onReadyForSpeech")
+    }
+
+    override fun onBeginningOfSpeech() {
+        Timber.d("onBeginningOfSpeech")
+    }
+
+    override fun onRmsChanged(rmsdB: Float) {
+    }
+
+    override fun onBufferReceived(buffer: ByteArray?) {
+    }
+
+    override fun onEndOfSpeech() {
+        Timber.d("onEndOfSpeech")
+    }
+
+    override fun onError(error: Int) {
+    }
+
+    override fun onResults(results: Bundle?) {
+        Timber.d("onResults")
+        Timber.d("${results!!.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)}")
+    }
+
+    override fun onPartialResults(partialResults: Bundle?) {
+        Timber.d("onPartialResults")
+        Timber.d("${partialResults!!.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)}")
+    }
+
+    override fun onEvent(eventType: Int, params: Bundle?) {
     }
 }
